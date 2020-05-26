5a2f943a839d9cb6751fb0bd4fee6501ce826f5d
MarcinParda <marcin98parda@gmail.com>
2020-05-26 16:04:19 +0200

    add login and logout functionality

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -4,7 +4,10 @@ import { returnErrors } from './messages';
 import {
     USER_LOADED,
     USER_LOADING,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS
 } from './types';
 
 // CHECK TYPES & LOAD USER
@@ -12,6 +15,64 @@ export const loadUser = () => (dispatch, getState) => {
     //User Loading
     dispatch({ type: USER_LOADING });
 
+    axios.get('/api/auth/user', tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: USER_LOADED,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({
+                type: AUTH_ERROR
+            })
+        })
+}
+
+// LOGIN USER
+export const login = (username, password) => dispatch => {
+
+    // Headers
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    // Request Body
+    const body = JSON.stringify({ username, password });
+
+    axios.post('/api/auth/login', body, config)
+        .then(res => {
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({
+                type: LOGIN_FAIL
+            })
+        })
+}
+
+// LOGOUT USER
+export const logout = () => (dispatch, getState) => {
+    axios.post('/api/auth/logout/', null, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LOGOUT_SUCCESS,
+            })
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+        })
+}
+
+// Setup Config with token - helper function
+export const tokenConfig = getState => {
     // Get token from state
     const token = getState().auth.token;
 
@@ -27,17 +88,5 @@ export const loadUser = () => (dispatch, getState) => {
         config.headers['Authorization'] = `Token ${token}`;
     }
 
-    axios.get('/api/auth/user', confirm)
-        .then(res => {
-            dispatch({
-                type: USER_LOADED,
-                payload: res.data
-            })
-        })
-        .catch(err => {
-            dispatch(returnErrors(err.response.data, err.response.status));
-            dispatch({
-                type: AUTH_ERROR
-            })
-        })
+    return config;
 }

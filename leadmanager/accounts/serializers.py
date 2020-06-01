12fca9e84a8dc3b9bb9ee14fa95787a494e03d96
Ketuael <ketuael@gmail.com>
2020-06-01 21:58:38 +0200

    Deleted unused Register Function, added some info to UserSerializer

--- leadmanager/accounts/serializers.py
+++ leadmanager/accounts/serializers.py
@@ -7,19 +7,7 @@ from django.contrib.auth import authenticate
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'is_staff')
-
-
-# Register Serializer
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
-        return user
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff')
 
 
 # Login Serializer

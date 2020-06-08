73316a734731659cf9745be46236f2e0c7a3a253
Ketuael <ketuael@gmail.com>
2020-06-09 01:00:20 +0200

    Turned on password validations

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
-""""
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-"""
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/

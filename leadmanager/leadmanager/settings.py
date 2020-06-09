fa80c7f2bf5d2ecda437e065f23908c3419622c0
MarcinParda <marcin98parda@gmail.com>
2020-06-09 10:01:00 +0200

    debug to false

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zed60qt+4o6%(5n*^vnv09)+jb^yk3&8xl(xr35mpgpff3#&uv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

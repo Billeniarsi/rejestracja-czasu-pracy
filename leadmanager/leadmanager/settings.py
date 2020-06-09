78a586b13e2650f2d4546b401b2adeb5de9d7dad
MarcinParda <marcin98parda@gmail.com>
2020-06-09 09:58:12 +0200

    allow every host

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zed60qt+4o6%(5n*^vnv09)+jb^yk3&8xl(xr35mpgpff3#&uv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
